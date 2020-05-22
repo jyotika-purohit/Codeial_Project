cd54faf4f7e8a4760fd1c8a32ec51fe3ffca9fb6
Jyotika Purohit <emailjyotikapurohit@gmail.com>
2020-05-22 15:55:33 +0530

    connect to mongoose and create user schema

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app=express();
 const port=8000;
 const path=require('path');
 const ejsLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
