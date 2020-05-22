6a582c2d5ba1e287100a26a69a60d73edb961893
Jyotika Purohit <emailjyotikapurohit@gmail.com>
2020-05-22 15:43:18 +0530

    create layout using express-ejs-layout

---
+++ controllers/homeController.js
@@ -0,0 +1,5 @@
+module.exports.homepage=function(req,res){
+    return res.render('home',{
+        title:'Codeail | Homepage'
+    });
+}
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const path=require('path');
+const ejsLayouts=require('express-ejs-layouts');
 
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+app.use(ejsLayouts);
+app.use('/',require('./routes/index'));
 app.listen(port,function(err){
     if(err){
         console.log("Error occurred while running the server",err);
