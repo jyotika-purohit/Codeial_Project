512c4b57554dac6af7515269c228ad85891c8f0a
Jyotika Purohit <emailjyotikapurohit@gmail.com>
2020-05-30 01:07:10 +0530

    create chatbox and sending messages and receiving messages

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -12,8 +12,95 @@ class ChatEngine{
     }
 
     connectionHandler(){
+        let self=this;
+        
+
         this.socket.on('connect',function(){
             console.log("Connection Established using sockets..");
-        })
+
+            self.socket.emit('join_room',{
+                user_email:self.userEmail,
+                chatroom:'codeial'
+            });
+
+            self.socket.on('user_joined',function(data){
+                console.log("A user joined",data);
+            });
+
+            $('#send-message-Btn').click(function(e){
+                e.preventDefault();
+
+                let newMessage=$('#new-message').val();
+                
+                if(newMessage!=''){
+                    self.socket.emit('send_message',{
+                        message:newMessage,
+                        user_Email:self.userEmail,
+                        chatroom:'codeial'
+                    });
+                }
+            });
+
+            $('#min-max-chatbox-Btn').click(function(e){
+                console.log($('#user-chat-box').css('height'));
+                if($('#user-chat-box').css('height')=='330px'){
+                    console.log($('#user-chat-box').css('height'));
+                    $('#user-chat-box').css({
+                        'height':'20px',
+                        'min-height': '20px',
+                        'overflow-y': 'hidden'
+                        
+                    });
+
+                    $('#new-message-form').css({
+                        'position':'static'
+                    });
+                    $('#min-max-chatbox-Btn').html('maximize');
+                }else{
+                    console.log($('#user-chat-box').css('height'));
+                    $('#user-chat-box').css({
+                        'height':'330px',
+                        'min-height': '330px',
+                        'overflow-y': 'scroll'
+                        
+                    });
+                    $('#new-message-form').css({
+                        'position':'fixed'
+                    });
+                    $('#min-max-chatbox-Btn').html('minimize');
+                }
+            });
+    
+            self.socket.on('receive_message',function(data){
+                console.log("message received");
+                console.log(data.message);
+    
+                let newMessage=$('<li>');
+    
+                let messageType='other-messages';
+    
+                if(data.user_Email == self.userEmail){
+                    messageType='my-messages';
+                }
+    
+                newMessage.append($('<span>',{
+                    'html':data.message
+                }));
+    
+                newMessage.append($('<h4>',{
+                    'html':data.user_Email
+                }));
+    
+                newMessage.addClass(messageType);
+    
+                $('#messages').append(newMessage);
+            });
+
+
+        });
+
+        
+
+
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,11 +1,24 @@
 module.exports.chatSockets=function(socketServer){
     let io=require('socket.io')(socketServer);
+
     io.sockets.on('connection',function(socket){
         console.log('newConnection received',socket.id); //socket is an object with prop of the user which is sending
 
         socket.on('disconnect',function(){
             console.log("Socket disconnected!");
-        })
-    })
+        });
+
+        socket.on('join_room',function(data){
+            console.log("Joining request",data);
+
+            socket.join(data.chatroom);  //chatroom is 'codeial'
+            
+            io.in(data.chatroom).emit('user_joined',data);  //user_joined is an event
+        });
+
+        socket.on('send_message',function(data) {
+            io.in(data.chatroom).emit('receive_message',data);
+        });
+    });
 
 }
